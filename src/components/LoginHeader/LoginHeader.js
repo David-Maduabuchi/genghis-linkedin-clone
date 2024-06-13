8fb9207faa9c458f3aca3bd4f07ae9a289e2e256
David Maduabuchi <davidmaduabuchi62@gmail.com>
2024-06-13 17:19:30 +0100

    Header Updates

--- src/components/LoginHeader/LoginHeader.js
+++ src/components/LoginHeader/LoginHeader.js
@@ -130,7 +130,7 @@ const LoginHeader = () => {
               <span className="games">Games</span>
             </a>
           </li>
-          <li>
+          <li className="getApp">
             <a href="http://" target="_blank" rel="noopener noreferrer">
               <svg
                 width={24}
@@ -146,12 +146,17 @@ const LoginHeader = () => {
               >
                 <path d="M21 17V8c0-1.66-1.34-3-3-3H6C4.34 5 3 6.34 3 8v9H1v1c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2v-1h-2zM11 6h2v1h-2V6zm8 11h-5c0 .55-.45 1-1 1h-2c-.55 0-1-.45-1-1H5V9c0-.55.45-1 1-1h12c.55 0 1 .45 1 1v8z"></path>
               </svg>
-              <span className="getApp">Get the app</span>
+              <span className="">Get the app</span>
             </a>
           </li>
         </ul>
       </div>
-      <div className="joinSignup"></div>
+      <div className="joinSignup">
+        <a href="http://">
+          Join Now
+        </a>
+        <button>Sign Up</button>
+      </div>
     </div>
   );
 };
